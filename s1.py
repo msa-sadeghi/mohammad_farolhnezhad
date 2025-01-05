fe275e8497e8145602dd1110ab8ee330b0f37777
msa-sadeghi <sadeghi.msai@gmail.com>
2025-01-05 17:57:07 +0330

    session 2

---
+++ s1.py
@@ -0,0 +1,37 @@
+# Variable
+# x = 0
+# print(x)
+
+# number = 1
+# print(number)
+
+
+# a = 2
+# b = 3
+# print(a+b)
+
+# number1 = 10
+# number2 = 6
+
+# print(number1 - number2)
+# print(number1 + number2)
+# print(number1 * number2)
+# print(number1 / number2)
+
+# string
+# name = "sina"
+# print(name)
+# name = 'sina'
+# print(name)
+
+
+name1 = "sara"
+name2 = "reza"
+name3 = "artin"
+
+print(name1 + " " +name2 + " " + name3)
+
+a = int(input("Enter a number "))
+b = int(input("Enter a number "))
+
+print(a + b)
