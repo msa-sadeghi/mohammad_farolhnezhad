8989b5a9155d1886c2eee2d62275ac457413d291
msa-sadeghi <sadeghi.msai@gmail.com>
2025-01-12 17:56:51 +0330

    f string added

---
+++ s3.py
@@ -0,0 +1,30 @@
+# مرور جلسه قبل
+# متغیر یا variable
+# x = 100
+# y = 200
+# print(x,y)
+
+# name = 'nima'
+# family = "rezaei"
+# print(name, family)
+
+
+# number1 = int(input("enter a number: "))
+# number2 = int(input("enter a number: "))
+
+# print(number1 + number2)
+
+# name = input("enter your name: ")
+# family = input("enter your family: ")
+# class_name = input("enter your course name: ")
+
+
+# message = f"Hello {name} {family}. Welcome to {class_name} class"
+# print(message)
+
+
+# number1 = float(input("enter a number: "))
+# number2 = float(input("enter a number: "))
+
+# print(f"{number1} + {number2} = {number1 + number2}")
+# print(f"{number1} - {number2} = {number1 - number2}")
